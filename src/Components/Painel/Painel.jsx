a5613d1ed2038741f5ba7cfdb7ebb761f41de29d
LuanaLucia <luanasantos@outlook.com>
2023-05-05 20:43:11 -0300

    useEffect

--- src/Components/Painel/Painel.jsx
+++ src/Components/Painel/Painel.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Painel.css"
 
 function Painel(props){
+    const [classe, setClasse] = useState("")
+
+    useEffect(()=>{
+        if (props.contagem > 0)
+            setClasse("destaque")
+    }, [props.contagem])
+
     return(
-        <div>
+        <div className={classe}>
             <span>{props.contagem}</span>
         </div>
     );
